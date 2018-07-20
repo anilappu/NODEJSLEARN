f019f198502b0bbab14673900e3cbf9406b78d8a
Anil Appukuttan <aappukuttan@lendingclub.com>
2018-07-20 10:33:06 -0700

    post to push messages

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+var messages = [
+    {name: 'Tim', message: 'Hi'},
+    {name: 'Jane', message: 'Hello'}
+]
+
+app.get('/messages',(req,res)=>{
+    res.send(messages)
+})
+
+app.post('/messages',(req,res)=>{
+    messages.push(req.body)
+    res.sendStatus(200)
+})
 
 app.use(express.static(__dirname))
 
